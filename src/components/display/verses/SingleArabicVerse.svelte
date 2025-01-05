d025c757480ade762a3e2f559c6f3c552bd91154
Marwan Ansari <8071158+marwan@users.noreply.github.com>
2025-01-05 22:52:24 +0530

    Update SingleArabicVerse.svelte

--- src/components/display/verses/SingleArabicVerse.svelte
+++ src/components/display/verses/SingleArabicVerse.svelte
@@ -2,20 +2,25 @@
 	export let key;
 
 	import Spinner from '$svgs/Spinner.svelte';
-	import { fetchVersesData } from '$utils/fetchData';
+	import { fetchChapterData } from '$utils/fetchData';
 	import { __fontType } from '$utils/stores';
-	import { splitDelimiter } from '$data/websiteSettings';
 
+	$: [chapter, verse] = key.split(':').map(Number);
 	$: fontType = [1, 2, 3].includes($__fontType) ? 1 : 4;
-	$: fetchData = fetchVersesData({ verses: key, fontType: fontType, skipSave: true });
+
+	$: fetchVerseData = fetchChapterData({
+		chapter: chapter,
+		fontType: fontType,
+		skipSave: true
+	});
 </script>
 
-{#await fetchData}
+{#await fetchVerseData}
 	<Spinner size="10" />
 {:then data}
 	<div class="direction-rtl text-3xl leading-normal arabic-font-{fontType}">
-		{data[key].words.arabic.split(splitDelimiter).join(' ')}
-		{data[key].words.end}
+		{data.arabic[verse].words.join(' ')}
+		{data.arabic[verse].end}
 	</div>
 {:catch error}
 	<p>error.</p>
