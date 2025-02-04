12e0da2cc81125e9ae2fa465b0238949d277df68
Marwan Ansari <8071158+marwan@users.noreply.github.com>
2025-02-04 23:06:29 +0530

    more event tracking

--- src/utils/updateSettings.js
+++ src/utils/updateSettings.js
@@ -249,7 +249,6 @@ export function updateSettings(props) {
 		case 'hideNonDuaPart':
 			__hideNonDuaPart.set(props.value);
 			userSettings.displaySettings.hideNonDuaPart = props.value;
-			trackEvent = true;
 			break;
 
 		// for quiz correct answers
